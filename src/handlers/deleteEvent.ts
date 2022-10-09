5b9a69aa0a3c3bb8b6c3b217d61356acb35b2e7e
Teilkasko <teilkasko@online.de>
2022-10-09 23:23:51 +0200

    rm tickets on delete event

--- src/handlers/deleteEvent.ts
+++ src/handlers/deleteEvent.ts
@@ -1,9 +1,15 @@
 import { RequestHandler } from 'express';
 import { deleteEvent } from '../model/event.js';
+import { deleteTicket, getTickets } from '../model/ticket.js';
 
 export const deleteEventHandler: RequestHandler = (req, res) => {
   const id = req.query['id'] as string;
   deleteEvent(id);
+
+  const affectedTicketIds = getTickets()
+    .filter((ticket) => ticket.eventId === id)
+    .map((ticket) => ticket.id);
+  affectedTicketIds.forEach((ticketId) => deleteTicket(ticketId));
 
   return res.status(200).send();
 };
