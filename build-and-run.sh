b42d1f9824af5830e045f8c2b6e55e345b99cd26
LinusAlbert.Scheibe <linusalbert.scheibe@stud-inf.unibz.it>
2022-12-03 23:58:26 +0100

    add build-and-run script

---
+++ build-and-run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "Now building and tagging the event service"
+
+cd event
+npm run build
+sudo docker build . -t teilkasko/tickent-event
+echo "Successfully tagged the event service"
+
+echo "Now building and tagging the ticket service"
+cd ../ticket
+npm run build
+sudo docker build . -t teilkasko/tickent-ticket
+echo "Successfully tagged the ticket service"
+
+cd ../docker-compose
+sudo docker-compose down
+sudo docker-compose up -d
+
+event_container_line=$(sudo docker container ls | grep event)
+event_id=${event_container_line:0:12}
+
+ticket_container_line=$(sudo docker container ls | grep ticket)
+ticket_id=${ticket_container_line:0:12}
+
+echo "Event container id: ${event_id}"
+echo "Ticket container id: ${ticket_id}"
